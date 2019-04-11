dfebadd5edfbcfe137f21c5c6814a0f13ec44ed2
Djaka Pradana <djakajaya89@gmail.com>
2019-04-11 23:24:51 +0700

    Added Profile, Seat, and Ticket Layout

--- app/src/main/java/id/djaka/flicker/ui/main/MainPresenter.kt
+++ app/src/main/java/id/djaka/flicker/ui/main/MainPresenter.kt
@@ -3,12 +3,16 @@ package id.djaka.flicker.ui.main
 import android.util.Log
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import id.djaka.flicker.ProfileFragment
 import id.djaka.flicker.R
+import id.djaka.flicker.TicketsFragment
 import id.djaka.flicker.ui.home.HomeFragment
 import id.djaka.mvpanddagger.base.BasePresenter
 
 class MainPresenter(mainView: MainView) : BasePresenter<MainView>(mainView){
     private var homeFragment: Fragment? = null
+    private var ticketFragment: Fragment? = null
+    private var profileFragment: Fragment? = null
 
     override fun onViewCreated() {
         super.onViewCreated()
@@ -26,11 +30,15 @@ class MainPresenter(mainView: MainView) : BasePresenter<MainView>(mainView){
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.navigation_dashboard -> {
-
+                    if(ticketFragment == null)
+                        ticketFragment = TicketsFragment()
+                    view.replaceFrame(ticketFragment, 1)
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.navigation_notifications -> {
-
+                    if(profileFragment == null)
+                        profileFragment = ProfileFragment()
+                    view.replaceFrame(profileFragment, 2)
                     return@OnNavigationItemSelectedListener true
                 }
             }
